9e3793a9d918f2f934b30b006e267eb4d0ddb3b8
Nirvik Bhadra Khanal <nbkhanal@deerwalk.com>
2017-05-05 17:20:45 +0545

    Initial commit

---
+++ src/myFirstJavaProgram/MyFirstJavaProgram.java
@@ -0,0 +1,10 @@
+package myFirstJavaProgram;
+
+/**
+ * Created by nbkhanal on 5/5/17.
+ */
+public class MyFirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello sansar");
+    }
+}
